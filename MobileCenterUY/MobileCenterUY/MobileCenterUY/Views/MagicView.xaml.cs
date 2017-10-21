e571d644a28b69d41ec3440dcd6bb3459d847821
Sebastian Cabrera <sergio.cabrera@uruit.com>
2017-10-21 14:23:38 -0300

    Added Simple and Magic view, Mobile Center packages

---
+++ MobileCenterUY/MobileCenterUY/MobileCenterUY/Views/MagicView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MobileCenterUY.Views
+{
+    public partial class MagicView : ContentPage
+    {
+        public MagicView()
+        {
+            InitializeComponent();
+        }
+
+        void BtnAnalytic_Clicked(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        void BtnCrash_Clicked(object sender, System.EventArgs e)
+        {
+            throw new Exception("My exception");
+        }
+    }
+}
