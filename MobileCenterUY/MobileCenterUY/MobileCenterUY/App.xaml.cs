9e368d67cdfd3d866ba8829f24b5c2aac8a4056e
adrianclaveri <adrian.claveri@uruit.com>
2017-10-25 14:18:46 -0300

    Feature/i os upload (#1)
    * no message
    
    * iOS Certificates
    
    * Added iOS p12 and Provisioning
    
    * Added Android keystore and package name in Droid project
    
    * Added Simple and Magic view, Mobile Center packages
    
    * Update in Crash and Analytic buttons
    
    * Added iOS and Android icons (all the resolutions)
    
    * Added Android splashscreen (for all the resolutions)
    
    * Updated iOS icons with white background
    
    * iOS splash and resources
    
    * Updated README
    
    * Changed config for firts half
    
    * Updated README-Release mode & Build status badges
    
    * Update README.md
    
    * Updated README - Aclaraciones
    
    * Updated Android icon for MC and Supporting more architectures for Release mode
    
    * Udpated README parte 1
    
    * Updated README
    
    * Wooala
    
    * Updated README
    
    * Updated README with Mobile Center link
    
    * test
    
    * Reverted test
    
    * Added pending netconf logos for Android resolutions
    
    * Final commit
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- MobileCenterUY/MobileCenterUY/MobileCenterUY/App.xaml.cs
+++ MobileCenterUY/MobileCenterUY/MobileCenterUY/App.xaml.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using MobileCenterUY.Views;
 
 using Xamarin.Forms;
 
@@ -9,11 +13,35 @@ namespace MobileCenterUY
 {
     public partial class App : Application
     {
+        private bool showMagicView = false;
+        private string mobileCenterKey_Android = "";
+        private string mobileCenterKey_iOS = "";
+
         public App()
         {
             InitializeComponent();
+            //Wooala();
+            MainPage = GetPage();
+        }
 
-            MainPage = new MobileCenterUY.MainPage();
+        private void Wooala()
+        {
+            showMagicView = true;
+            StartMobileCenterSdk();
+        }
+
+        private void StartMobileCenterSdk()
+        {
+            if (!string.IsNullOrWhiteSpace(mobileCenterKey_Android) || !string.IsNullOrWhiteSpace(mobileCenterKey_iOS))
+            {
+                string startString = string.Format("android={0};" + "ios={1}", mobileCenterKey_Android, mobileCenterKey_iOS);
+                MobileCenter.Start(startString, typeof(Analytics), typeof(Crashes));
+            }
+        }
+
+        private ContentPage GetPage()
+        {
+            return showMagicView ? (ContentPage)new MagicView() : (ContentPage)new SimpleView();
         }
 
         protected override void OnStart()
---
+++ MobileCenterUY/MobileCenterUY/MobileCenterUY/Views/SimpleView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MobileCenterUY.Views
+{
+    public partial class SimpleView : ContentPage
+    {
+        public SimpleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
